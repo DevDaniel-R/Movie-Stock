177c069b7c6417424bbd6afe5e23ccd6ba44a112
Daniel Rodriguez <drodriguez661@gmail.com>
2020-01-20 19:38:09 -0600

    creating custom hook

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -1,20 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { API_URL,
-   API_KEY,
-   API_BASE_URL,
-   POSTER_SIZE,
-   BACKDROP_SIZE} 
-   from '../config';
+import { useState, useEffect } from 'react';
+import { API_URL, API_KEY } from '../../config';
 
-//import Components
-import HeroImage from './elements/HeroImage';
-import SearchBar from './elements/SearchBar';
-import Grid from './elements/Grid';
-import MovieThumb from './elements/MovieThumb';
-import LoadMoreBtn from './elements/LoadMoreBtn';
-import Spinner from './elements/Spinner';
-
-const Home = () => {
+export const useHomeFetch = () => {
   const [state, setState] = useState({ movies: [] });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -48,19 +35,4 @@ const Home = () => {
   useEffect(() => {
     fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`)
   }, [])
-
-
-  return (
-
-    <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <Spinner />  
-    <LoadMoreBtn />
-    </>
-  )
 }
-
-export default Home;
